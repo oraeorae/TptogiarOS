d73366a2b0ccb1efed9015681e4f721747f10271
Tptogiar <2528891112@qq.com>
2022-08-01 21:12:16 +0800

    实现通用的双向链表

--- kernel/c/headers/func/list.h
+++ kernel/c/headers/func/list.h
@@ -32,16 +32,53 @@ static inline void listPickOff(ListEntry* prev,ListEntry next) __attribute__((al
 
 
 
+satic inline void listInit(ListEntry* elm){
+    elm->prev = elm->next =elm;
+}
 
+static inline void listAppend(ListEntry* listElm,ListEntry* elm){
+    listaddAfter(listElm,elm);
+}
 
+static inline void listaddBefore(ListEntry* listElm,ListEntry* elm){
+    listInsert(elm,listElm->prev,listElm);
+}
 
+static inline void listaddAfter(ListEntry* listElm,ListEntry* elm){
+    listInsert(elm,listElm,listElm->next);
+}
 
+static inline void listDel(ListEntry* listElm){
+   listPickOff(listElm->prev,listElm->next);
+}
 
+static inline void listDelInit(ListEntry* listElm){
+    listDel(listElm);
+    listInit(listElm);
+}
 
+static inline bool listEmpty(ListEntry* list){
+    return list->next == list;
+}
 
+static inline ListEntry* listNext(ListEntry* listElm){
+    return listElm->next;
+}
 
+static inline ListEntry* listPrev(ListEntry* listElm){
+    return listElm->prev;
+}
 
+static inline void listInsert(ListEntry* elm,ListEntry* prev,ListEntry* next){
+    prev->next = elm = next->prev;
+    elm->prev =  prev;
+    elm->next = next;
+}
 
+static inline void listPickOff(ListEntry* prev,ListEntry next){
+    prev->next = next;
+    next.prev = prev;
+}
 
 
 #endif //TPTOGIAROS_LIST_H
