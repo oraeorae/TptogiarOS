924570be7e158e1fc303384c922cc5d0548df26f
Tptogiar <2528891112@qq.com>
2022-08-03 11:44:11 +0800

    【最小可执行内核】

---
+++ rv64/kernel/memoryMgr/mmu.h
@@ -0,0 +1,15 @@
+//
+// Created by root on 2022.08.02.
+//
+
+#ifndef TPTOGIAROS_MMU_H
+#define TPTOGIAROS_MMU_H
+
+
+// 页大小
+#define PGSIZE 4096
+// 12位对其，即4KB
+#define PGSHIFT 12
+
+
+#endif //TPTOGIAROS_MMU_H
