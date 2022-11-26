152743e198ad6c343a3b3cd317c65439f8a6de30
Tptogiar <2528891112@qq.com>
2022-11-26 22:32:45 +0800

    stack test pass

--- rv64/tools/container/stack.c
+++ rv64/tools/container/stack.c
@@ -1,1 +1,36 @@
+#include "stack.h"
+
+
+void stack_construct(stack_t* stack, size_t type_size)
+{
+    link_construct(&stack->link, type_size);
+}
+
+void stack_init_alloc_dealloc(stack_t* stack, void* (*alloc) (size_t), void (*dealloc) (void*))
+{
+    link_init_alloc_dealloc(&stack->link,alloc, dealloc);
+}
+
+void stack_push(stack_t* stack, void* v)
+{
+    link_push(&stack->link, v);
+}
+
+void stack_pop(stack_t* stack, void* data)
+{
+    link_node_t* node = link_end(&stack->link);
+    type_copy(node->data, data, stack->link.type_size);
+    link_delete(&stack->link, node);
+}
+
+size_t stack_length(stack_t* stack)
+{
+    return link_length(&stack->link);
+}
+
+void stack_destory(stack_t* stack)
+{
+    link_destory(&stack->link);
+}
+
 
--- rv64/tools/container/stack.h
+++ rv64/tools/container/stack.h
@@ -1,6 +1,34 @@
-
-
 #ifndef TPTOGIAROS_STACK_H
 #define TPTOGIAROS_STACK_H
+
+#pragma once
+
+#include "link.h"
+
+typedef struct {
+    link_t link;
+}stack_t;
+
+
+
+void stack_construct(stack_t* stack, size_t type_size);
+
+void stack_init_alloc_dealloc(stack_t* stack, void* (*alloc) (size_t), void (*dealloc) (void*));
+
+void stack_push(stack_t* stack, void* v);
+
+void stack_pop(stack_t* stack, void* data);
+
+size_t stack_length(stack_t* stack);
+
+void stack_destory(stack_t* stack);
+
+bool stack_empty(stack_t* stack);
+
+
+
+
+
+
 
 #endif //TPTOGIAROS_STACK_H
